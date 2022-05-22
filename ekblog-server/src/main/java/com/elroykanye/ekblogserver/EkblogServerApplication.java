fb4688074cac9fb9a5d98e7f77412cd083655f86
Elroy Kanye <elroykanye@gmail.com>
2022-05-22 13:07:28 +0100

    added server module
    added ekblog server
    
    added ekblog server
    
    added server module

---
+++ ekblog-server/src/main/java/com/elroykanye/ekblogserver/EkblogServerApplication.java
@@ -0,0 +1,13 @@
+package com.elroykanye.ekblogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkblogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EkblogServerApplication.class, args);
+    }
+
+}
---
+++ ekblog-server/src/test/java/com/elroykanye/ekblogserver/EkblogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elroykanye.ekblogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EkblogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
