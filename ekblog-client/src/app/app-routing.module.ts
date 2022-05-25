fe3aff8fc04b13af1ed06ef04a02742fd2853704
Elroy Kanye <elroykdev@gmail.com>
2022-05-25 21:26:06 +0100

    add blog post component to routes

--- ekblog-client/src/app/app-routing.module.ts
+++ ekblog-client/src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {EkHomeComponent} from "./components/ek-home/ek-home.component";
 import {EkContactComponent} from "./components/ek-contact/ek-contact.component";
 import {EkBlogComponent} from "./components/ek-blog/ek-blog.component";
 import {EkPortfolioComponent} from "./components/ek-portfolio/ek-portfolio.component";
 import {EkAboutComponent} from "./components/ek-about/ek-about.component";
+import {EkBlogPostComponent} from "./components/ek-blog-post/ek-blog-post.component";
 
 const routes: Routes = [
   {path: '', component: EkHomeComponent},
   {path: 'about', component: EkAboutComponent},
   {path: 'contact', component: EkContactComponent},
-  {path: 'blog', component: EkBlogComponent},
+  {
+    path: 'blog',
+    component: EkBlogComponent,
+    children: []
+  },
+  {path: 'post/:id', component: EkBlogPostComponent},
   {path: 'portfolio', component: EkPortfolioComponent}
 ];
 
