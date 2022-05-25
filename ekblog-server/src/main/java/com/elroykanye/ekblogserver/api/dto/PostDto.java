07497be2472bd88cf061fb85c2888f779d6014c5
Elroy Kanye <elroykdev@gmail.com>
2022-05-25 21:39:50 +0100

    update date format for dtos

--- ekblog-server/src/main/java/com/elroykanye/ekblogserver/api/dto/PostDto.java
+++ ekblog-server/src/main/java/com/elroykanye/ekblogserver/api/dto/PostDto.java
@@ -1,8 +1,20 @@
 package com.elroykanye.ekblogserver.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 
-public record PostDto(Long id, String imageUrl, String title, Integer numberOfComments,
-                      List<PostSectionDto> sections) implements Serializable {
+public record PostDto(
+        Long id,
+        String imageUrl,
+        String title,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime createdAt,
+        @NotEmpty(message = "Post must have at least 1 section")
+        List<PostSectionDto> sections,
+        Integer numberOfComments
+) implements Serializable {
 }
--- ekblog-server/src/main/java/com/elroykanye/ekblogserver/api/dto/PostSectionDto.java
+++ ekblog-server/src/main/java/com/elroykanye/ekblogserver/api/dto/PostSectionDto.java
@@ -1,9 +1,14 @@
 package com.elroykanye.ekblogserver.api.dto;
 
 import com.elroykanye.ekblogserver.data.SectionType;
-import lombok.Data;
 
 import java.io.Serializable;
 
-public record PostSectionDto(Long id, Integer order, String content, SectionType sectionType, Long postId)
-        implements Serializable { }
+public record PostSectionDto(
+        Long id,
+        Integer order,
+        String content,
+        SectionType sectionType,
+        Long postId
+) implements Serializable {
+}
