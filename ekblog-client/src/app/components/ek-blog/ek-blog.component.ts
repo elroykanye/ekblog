95ea344ed5dc3314ba5b9027c05b0eec0e7ab6d3
Elroy Kanye <elroykdev@gmail.com>
2022-05-25 13:51:02 +0100

    update app arrangement

--- ekblog-client/src/app/components/ek-blog/ek-blog.component.ts
+++ ekblog-client/src/app/components/ek-blog/ek-blog.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ek-blog.component.scss']
 })
 export class EkBlogComponent implements OnInit {
-
+  homePosts: {image: string; title: string; introText: string}[] = [
+    {
+      image: "assets/img/blog-post-1.jpg",
+      title: "How to get started with Angular",
+      introText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis..."
+    },
+    {
+      image: "assets/img/blog-post-1.jpg",
+      title: "How to get started with Angular",
+      introText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis..."
+    },
+    {
+      image: "assets/img/blog-post-1.jpg",
+      title: "How to get started with Angular",
+      introText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis..."
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- ekblog-client/src/app/components/ek-home/ek-home.component.ts
+++ ekblog-client/src/app/components/ek-home/ek-home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {EK_SERVICES, EK_SOCIALS} from "../../app.constants";
+
+declare var MainJs: any;
 
 @Component({
   selector: 'app-ek-home',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ek-home.component.scss']
 })
 export class EkHomeComponent implements OnInit {
+  services = EK_SERVICES;
 
   constructor() { }
 
   ngOnInit(): void {
+    new MainJs();
   }
 
 }
