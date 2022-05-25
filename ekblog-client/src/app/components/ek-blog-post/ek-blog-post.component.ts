af5ddcf43444e2d6a5393ef905760aaee8f74222
Elroy Kanye <elroykdev@gmail.com>
2022-05-25 21:31:43 +0100

    inject and use post and comment service in blog and post

--- ekblog-client/src/app/components/ek-blog-post/ek-blog-post.component.ts
+++ ekblog-client/src/app/components/ek-blog-post/ek-blog-post.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Post} from "../../models/post";
+import {ActivatedRoute} from "@angular/router";
+import {PostService} from "../../services/post.service";
+import {PostComment} from "../../models/post-comment";
+import {PostCommentService} from "../../services/post-comment.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {EkbResponse} from "../../models/ekb-response";
 
 @Component({
   selector: 'app-ek-blog-post',
@@ -7,9 +14,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EkBlogPostComponent implements OnInit {
 
-  constructor() { }
+  post: Post = {id: 0, title: '', imageUrl: '', sections: []};
+  comments: PostComment[] = [];
+  commentForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private postService: PostService, private postCommentService: PostCommentService) {
+    this.commentForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl(''),
+      website: new FormControl(''),
+      content: new FormControl('', Validators.required)
+    })
   }
 
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const postId = Number(routeParams.get('id'));
+    this.loadPost(postId);
+    this.loadComments(postId);
+  }
+
+  loadPost(postId: number): void {
+    this.postService.getPostById(postId).subscribe({
+      next: (post: Post) => {
+        this.post = post;
+        console.log(this.post.sections[0].sectionType.toString())
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  loadComments(postId: number): void {
+    this.postCommentService.getPostComments(postId).subscribe({
+      next: (comments: PostComment[]) => {
+        this.comments = comments;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  addComment(comment: PostComment): void {
+
+    console.log(comment)
+    this.postCommentService.addPostComment(comment).subscribe({
+      next: (res: EkbResponse) => {
+        this.comments.push(comment);
+        this.loadComments(comment.postId);
+        console.log(res)
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+
+  }
+
+  addCommentAction() {
+    const comment: PostComment = {
+      id: -1,
+      postId: this.post.id,
+      content: this.commentForm.get('content')?.value,
+      authorName: this.commentForm.get('name')?.value,
+      authorEmail: this.commentForm.get('email')?.value,
+    }
+    this.addComment(comment);
+  }
 }
--- ekblog-client/src/app/components/ek-blog/ek-blog.component.ts
+++ ekblog-client/src/app/components/ek-blog/ek-blog.component.ts
@@ -1,31 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostService} from "../../services/post.service";
+import {DEFAULT_POSTS_DISPLAY, Post, PostDisplay, PostUtil} from "../../models/post";
 
 @Component({
   selector: 'app-ek-blog',
-  templateUrl: './ek-blog.component.html',
-  styleUrls: ['./ek-blog.component.scss']
+  styleUrls: ['./ek-blog.component.scss'],
+  template: `
+    <div id="journal" class="text-left paddsection">
+      <div class="container">
+        <div class="section-title text-center">
+          <h2>journal</h2>
+        </div>
+      </div>
+      <div class="container">
+        <div class="journal-block">
+          <div class="row">
+            <div class="col-lg-4 col-md-6" *ngFor="let post of displayPosts">
+              <div class="journal-info">
+                <a [routerLink]="['/post', post.id]">
+                  <img src="{{post.image}}" class="img-responsive" alt="img">
+                </a>
+                <div class="journal-txt">
+                  <h4><a [routerLink]="['/post', post.id]">{{post.title}}</a></h4>
+                  <p class="separator">{{post.introText}}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
 })
 export class EkBlogComponent implements OnInit {
-  homePosts: {image: string; title: string; introText: string}[] = [
-    {
-      image: "assets/img/blog-post-1.jpg",
-      title: "How to get started with Angular",
-      introText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis..."
-    },
-    {
-      image: "assets/img/blog-post-1.jpg",
-      title: "How to get started with Angular",
-      introText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis..."
-    },
-    {
-      image: "assets/img/blog-post-1.jpg",
-      title: "How to get started with Angular",
-      introText: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis..."
-    }
-  ];
-  constructor() { }
+  displayPosts: PostDisplay[] = DEFAULT_POSTS_DISPLAY;
+  posts: Post[] = [];
+
+  constructor(private postService: PostService) {
+  }
 
   ngOnInit(): void {
+    this.postService.getAllPosts().subscribe({
+      next: (posts) => {
+        this.posts = posts;
+        console.log(posts)
+        this.processPosts(posts)
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  processPosts(posts: Post[]) {
+    this.displayPosts = PostUtil.getPostDisplays(posts);
   }
 
 }
