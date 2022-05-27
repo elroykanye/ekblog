58dd4a9e4ecd0e16951b80aee362cc6c38c593cb
Elroy Kanye <elroykdev@gmail.com>
2022-05-27 22:59:09 +0100

    add ekb controller

---
+++ ekblog-server/src/main/java/com/elroykanye/ekblogserver/api/controller/EkbController.java
@@ -0,0 +1,17 @@
+package com.elroykanye.ekblogserver.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/")
+public class EkbController {
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String home() {
+        return "forward:index.html";
+    }
+}
