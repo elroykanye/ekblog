0b7d6128104c3a50a7dd6611b5b257b894512506
Elroy Kanye <elroykdev@gmail.com>
2022-05-28 09:15:03 +0100

    add client methods for loading add/delete post

--- ekblog-client/src/app/services/post.service.ts
+++ ekblog-client/src/app/services/post.service.ts
@@ -2,6 +2,7 @@ import {Inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Post} from "../models/post";
+import {EkbResponse} from "../models/ekb-response";
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,19 @@ export class PostService {
   constructor(@Inject("EKB_POST_API_URL") private postApiUrl: string, private http: HttpClient) {
   }
 
+  createPost(post: Post): Observable<EkbResponse> {
+    return this.http.post<EkbResponse>(this.postApiUrl, post);
+  }
+
   getAllPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.postApiUrl);
   }
 
   getPostById(id: number): Observable<Post> {
     return this.http.get<Post>(`${this.postApiUrl}/${id}`);
+  }
+
+  deletePostById(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.postApiUrl}/${id}`);
   }
 }
